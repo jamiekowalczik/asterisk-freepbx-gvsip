3704a765c1e2d8dda28d8c5bf2ffdf8a3ac3537b
jamie.kowalczik@gmail.com <jamie.kowalczik@gmail.com>
2018-08-06 22:23:08 -0400

    fixed pjsip_custom.conf

--- provision/install.bash
+++ provision/install.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 ## Install as root
-## bash <(curl -s https://raw.githubusercontent.com/jamiekowalczik/asterisk-freepbx/master/provision/install.bash)
+## bash <(curl -s https://raw.githubusercontent.com/jamiekowalczik/asterisk-freepbx-gvsip/master/provision/install.bash)
 yum -y install git python-virtualenv
 virtualenv ansible
 . ./ansible/bin/activate
 pip install pip -U
 pip install setuptools -U
 pip install ansible
-git clone https://github.com/jamiekowalczik/asterisk-freepbx.git
+git clone https://github.com/jamiekowalczik/asterisk-freepbx-gvsip.git
 cd asterisk-freepbx/provision
 echo ""
 read -p "Update asterisk-freepbx/provision/freepbx-gvsip-el7.yml to include your GVSIP details then press enter to continue"
