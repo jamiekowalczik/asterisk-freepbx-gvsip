fd7c840e03f859b15325752b2b2de2099dd21516
jamie.kowalczik@gmail.com <jamie.kowalczik@gmail.com>
2018-08-10 23:12:45 -0400

    fixed install.bash script

--- provision/install.bash
+++ provision/install.bash
@@ -10,5 +10,5 @@ pip install ansible
 git clone https://github.com/jamiekowalczik/asterisk-freepbx-gvsip.git
 cd asterisk-freepbx-gvsip/provision
 echo ""
-read -p "Update asterisk-freepbx/provision/freepbx-gvsip-el7.yml to include your GVSIP details then press enter to continue"
+read -p "Update asterisk-freepbx-gvsip/provision/freepbx-gvsip-el7.yml to include your GVSIP details then press enter to continue"
 ansible-playbook freepbx-gvsip-el7.yml -i inventory
